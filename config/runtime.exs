619dc6366554f12a3c4d3d37f69600a0c5af2805
Jon Rowe <mail@jonrowe.co.uk>
2024-01-24 22:04:58 +0000

    Add token

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,7 +5,8 @@ tokens = %{
   "2024-01-24" => "zt-216sp1w4g-MHgJlFeJKTBCzPejZj7_tw",
   "2024-01-24" => "zt-2b8vocbsl-8HPQL3vchrSZGt_I4ypMsw",
   "2024-01-24" => "zt-2bklcc3k0-VBGZE6s32lF7HyPBCoZ40A",
-  "2024-01-24" => "zt-2b75947fa-1UHbh8s4d_y~4vEhsX20~Q"
+  "2024-01-24" => "zt-2b75947fa-1UHbh8s4d_y~4vEhsX20~Q",
+  "2024-01-24" => "zt-2apqof3wh-QVSNtcdERC8YkY99leoWyQ"
 }
 
 config :elixir_slack,
