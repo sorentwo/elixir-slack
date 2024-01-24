759aabbac528dfa7fb07b701b63c3ae66934a93f
Parker Selbert <parker@sorentwo.com>
2024-01-24 15:55:09 -0600

    Add another link and switch to date/map format

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,8 +1,12 @@
 import Config
 
+# Tokens are listed with the date they were added to make pruning old, "dead" links easier.
+tokens = %{
+  "2024-01-24" => "zt-216sp1w4g-MHgJlFeJKTBCzPejZj7_tw",
+  "2024-01-24" => "zt-2bklcc3k0-VBGZE6s32lF7HyPBCoZ40A",
+  "2024-01-24" => "zt-2b75947fa-1UHbh8s4d_y~4vEhsX20~Q"
+}
+
 config :elixir_slack,
   port: String.to_integer(System.get_env("PORT", "8000")),
-  invite_tokens: ~w(
-    zt-216sp1w4g-MHgJlFeJKTBCzPejZj7_tw
-    zt-2bklcc3k0-VBGZE6s32lF7HyPBCoZ40A
-  )
+  invite_tokens: Map.values(tokens)
