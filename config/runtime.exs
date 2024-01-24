ab5b90999418137881cff6f4248ab01febed5109
Parker Selbert <parker@sorentwo.com>
2024-01-24 15:36:03 -0600

    Hard code invite tokens instead of using system
    Stored secrets can't be viewed through the fly cli, and they're not
    really secret because it's shown in the redirect.

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,11 +1,8 @@
 import Config
 
-invite_tokens =
-  "INVITE_TOKENS"
-  |> System.get_env("")
-  |> String.split(~r/\s*/)
-
 config :elixir_slack,
-  invite_tokens: invite_tokens,
-  port: String.to_integer(System.get_env("PORT", "8000"))
-
+  port: String.to_integer(System.get_env("PORT", "8000")),
+  invite_tokens: ~w(
+    zt-216sp1w4g-MHgJlFeJKTBCzPejZj7_tw
+    zt-2bklcc3k0-VBGZE6s32lF7HyPBCoZ40A
+  )
