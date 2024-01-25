729e4f8f6c96c2f645fe14ea9e2989ead5a42bfb
Herminio Torres <herminiocesar@gmail.com>
2024-01-24 21:57:24 -0300

    Update runtime.exs (#3)

--- config/runtime.exs
+++ config/runtime.exs
@@ -8,7 +8,8 @@ tokens = %{
   "zt-216sp1w4g-MHgJlFeJKTBCzPejZj7_tw" => "2024-01-24",
   "zt-2bklcc3k0-VBGZE6s32lF7HyPBCoZ40A" => "2024-01-24",
   "zt-2b75947fa-1UHbh8s4d_y~4vEhsX20~Q" => "2024-01-24",
-  "zt-2b8vocbsl-8HPQL3vchrSZGt_I4ypMsw" => "2024-01-24"
+  "zt-2b8vocbsl-8HPQL3vchrSZGt_I4ypMsw" => "2024-01-24",
+  "zt-2b90hwr0l-KzkegyzRL1XgmA6Q~CAlIQ" => "2024-01-24"
 }
 
 config :elixir_slack,
